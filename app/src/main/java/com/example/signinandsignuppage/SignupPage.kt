a730215b9e94188f5f12fc3ed1cb43938d492eea
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-11-15 14:23:04 +0300

    Sign up page are created.

--- app/src/main/java/com/example/signinandsignuppage/SignupPage.kt
+++ app/src/main/java/com/example/signinandsignuppage/SignupPage.kt
@@ -1,15 +1,11 @@
 package com.example.signinandsignuppage
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignupPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        startActivity(Intent(this,SignupPage::class.java))
-        finish()
+        setContentView(R.layout.activity_signup_page)
     }
 }
